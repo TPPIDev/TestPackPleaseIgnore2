d3e1b89b5e01f8bd86395a6b72c3484bfc0cf613
Nick <nick_ignoffo@yahoo.com>
2015-02-15 12:38:04 -0800

    Add BiblioWoods to NEI culling

--- config/INpureProjects/custom_nei_filters/Bibliocraft.js
+++ config/INpureProjects/custom_nei_filters/Bibliocraft.js
@@ -1,3 +1,15 @@
 if (FML.isModLoaded("BiblioCraft") && Bibliocraft_enabled){
     NEI.override("BiblioCraft:*", [0]);
 }
+
+if (FML.isModLoaded("BiblioWoodsNatura") {
+	NEI.override("BiblioWoodsNatura:*", [0]);
+}
+
+if (FML.isModLoaded("BiblioWoodsBoP") {
+	NEI.override("BiblioWoodsBoP:*", [0]);
+}
+
+if (FML.isModLoaded("BiblioWoodsForestry") {
+	NEI.override("BiblioWoodsForestry:*", [0]);
+}
