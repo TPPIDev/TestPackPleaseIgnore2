5ab63b702a35d452b286c5b8db32a11772ef5b32
Nick <nick_ignoffo@yahoo.com>
2015-02-15 12:38:46 -0800

    Make them respect the config. Oopsy

--- config/INpureProjects/custom_nei_filters/Bibliocraft.js
+++ config/INpureProjects/custom_nei_filters/Bibliocraft.js
@@ -2,14 +2,14 @@ if (FML.isModLoaded("BiblioCraft") && Bibliocraft_enabled){
     NEI.override("BiblioCraft:*", [0]);
 }
 
-if (FML.isModLoaded("BiblioWoodsNatura") {
+if (FML.isModLoaded("BiblioWoodsNatura" && Bibliocraft_enabled) {
 	NEI.override("BiblioWoodsNatura:*", [0]);
 }
 
-if (FML.isModLoaded("BiblioWoodsBoP") {
+if (FML.isModLoaded("BiblioWoodsBoP" && Bibliocraft_enabled) {
 	NEI.override("BiblioWoodsBoP:*", [0]);
 }
 
-if (FML.isModLoaded("BiblioWoodsForestry") {
+if (FML.isModLoaded("BiblioWoodsForestry" && Bibliocraft_enabled) {
 	NEI.override("BiblioWoodsForestry:*", [0]);
 }
