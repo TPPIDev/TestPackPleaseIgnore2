58677b4d8714df841d7debf2d8fad8bd88e8ae1b
Nick <nick_ignoffo@yahoo.com>
2015-02-15 12:49:12 -0800

    Proofreading. Do it. Pro life tip

--- config/INpureProjects/custom_nei_filters/Bibliocraft.js
+++ config/INpureProjects/custom_nei_filters/Bibliocraft.js
@@ -1,15 +1,15 @@
-if (FML.isModLoaded("BiblioCraft") && Bibliocraft_enabled){
+if (FML.isModLoaded("BiblioCraft") && Bibliocraft_enabled) {
     NEI.override("BiblioCraft:*", [0]);
 }
 
-if (FML.isModLoaded("BiblioWoodsNatura" && Bibliocraft_enabled) {
+if (FML.isModLoaded("BiblioWoodsNatura") && Bibliocraft_enabled) {
 	NEI.override("BiblioWoodsNatura:*", [0]);
 }
 
-if (FML.isModLoaded("BiblioWoodsBoP" && Bibliocraft_enabled) {
+if (FML.isModLoaded("BiblioWoodsBoP") && Bibliocraft_enabled) {
 	NEI.override("BiblioWoodsBoP:*", [0]);
 }
 
-if (FML.isModLoaded("BiblioWoodsForestry" && Bibliocraft_enabled) {
+if (FML.isModLoaded("BiblioWoodsForestry") && Bibliocraft_enabled) {
 	NEI.override("BiblioWoodsForestry:*", [0]);
 }
